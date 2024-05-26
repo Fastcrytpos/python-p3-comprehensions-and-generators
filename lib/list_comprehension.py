a860baaf14a2b0b817f6c7ae91962cdf25c5133e
Samson Mubeya <rasspartah@gmail.com>
2024-05-26 21:22:56 +0300

    learnt comprehensions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+        list_of_evens=[n for n in num_list if n % 2==0 ]
+        return list_of_evens if list_of_evens else []
+
+    
 
 def make_exclamation(sentence_list):
+    result=[] if not sentence_list else [str(n)+"!" for n in sentence_list]
+    return result
     pass
